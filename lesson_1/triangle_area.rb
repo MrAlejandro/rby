162d55a40585985d077a9cce2d2bcc87d316c91b
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-01-21 22:22:02 +0400

    Lesson 1. Triangle area task

---
+++ lesson_1/triangle_area.rb
@@ -0,0 +1,8 @@
+puts "Enter triangle base length: "
+base = gets.chomp.to_i
+
+puts "Enter triangle height: "
+height = gets.chomp.to_i
+
+area = 0.5 * base * height
+puts "The triangle's area equals to #{area}"
