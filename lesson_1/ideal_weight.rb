1d998159c65a23e814170ac7c8568faaf93eca48
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-01-21 23:54:59 +0400

    Lesson 1. ideal weight convestion to float added

--- lesson_1/ideal_weight.rb
+++ lesson_1/ideal_weight.rb
@@ -2,14 +2,14 @@ puts "What's your name? "
 name = gets.chomp
 
 puts "What's your height? "
-height = gets.chomp.to_i
+height = gets.chomp.to_f
 
 puts "What's your weight"
-weight = gets.chomp.to_i
+weight = gets.chomp.to_f
 
 ideal_weight = height - 110
 if ideal_weight < weight
-  puts "#{name}, your ideal weight is #{ideal_weight}"
+  puts "#{name}, your ideal weight is #{ideal_weight.round(2)}"
 else
   puts "Your weight is already optimal"
 end
