1bf855373a50c444cce46ee5e8dfcee96b3b53d9
Aleksandr Scherbachenko <ascherbachenko@cs-cart.com>
2019-01-25 13:49:51 +0400

    Lesson 3. Routes implementation slightly reworked.

--- lesson_3/route.rb
+++ lesson_3/route.rb
@@ -4,40 +4,33 @@ class Route
   def initialize(initial_station, terminal_station)
     @initial_station = initial_station
     @terminal_station = terminal_station
-    @way_stations = []
+    @stations = [initial_station, terminal_station]
   end
 
   def add_station(station)
-    already_exists = @initial_station == station || @terminal_station == station || @way_stations.include?(station)
-    @way_stations << station unless already_exists
+    already_exists = @initial_station == station || @terminal_station == station || @stations.include?(station)
+    @stations.insert(-2, station) unless already_exists
     self
   end
 
   def remove_station(station)
-    @way_stations.delete(station) unless station.has_trains?
+    is_edge_station = station == @initial_station || station == @terminal_station
+    can_remove = !is_edge_station && !station.has_trains?
+    @stations.delete(station) if can_remove
   end
 
   def print_stations
-    get_all_stations.each { |station| puts station.to_str }
+    @stations.each { |station| puts station.to_str }
   end
 
   def get_station_after(station)
-    station_index = get_index_of_station(station)
-    station_index ? get_all_stations[station_index+1] : nil
+    station_index = @stations.index(station)
+    station_index ? @stations[station_index+1] : nil
   end
 
   def get_station_before(station)
-    station_index = get_index_of_station(station)
-    station_index && station_index > 0 ? get_all_stations[station_index-1] : nil
-  end
-
-  private
-  def get_all_stations
-    [@initial_station, @way_stations, @terminal_station].flatten
-  end
-
-  def get_index_of_station(station)
-    get_all_stations.find_index(station)
+    station_index = @stations.index(station)
+    station_index && station_index > 0 ? @stations[station_index-1] : nil
   end
 end
 
